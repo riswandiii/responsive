623c84303e40e48bd4dbfac9bde55488c74c28bc
riswandiii <riswandiandi017@gmail.com>
2022-04-16 00:28:17 +0700

    Responsive

---
+++ grid.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Grid System</title>
+  </head>
+  <body>
+  
+  <div class="container-fluid bg-dark">
+      <div class="container">
+          <div class="row py-3">
+              <div class="col-12">
+                  <h3 class="text-center text-white">BELAJAR GRID SYSTEM</h3>
+              </div>
+          </div>
+          <div class="row justify-content-center">
+              <div class="col-lg-6 col-md-6">
+                  <div class="text-white"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis unde cum nam libero fugiat, accusamus voluptatem ipsam laudantium placeat deleniti saepe, natus vero eum accusantium recusandae similique assumenda beatae culpa quae rem aut deserunt quasi consectetur omnis. Minus, adipisci, quibusdam saepe repellendus fuga ipsa repudiandae, similique non nam soluta accusamus?</p></div>
+              </div>
+              <div class="col-lg-5 col-md-6 offset-lg-1">
+                  <div class="text-white"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis unde cum nam libero fugiat, accusamus voluptatem ipsam laudantium placeat deleniti saepe, natus vero eum accusantium recusandae similique assumenda beatae culpa quae rem aut deserunt quasi consectetur omnis. Minus, adipisci, quibusdam saepe repellendus fuga ipsa repudiandae, similique non nam soluta accusamus?</p></div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <div class="container mt-5">
+
+      <div class="row">
+          <div class="col-12">
+              <div class=" text-center">
+                  <h3>My Gallery</h3>
+                  <p>Collection Foto Since 2021-2022</p>
+              </div>
+          </div>
+      </div>
+
+      <div class="container-fluid">
+      <div class="row">
+            <div class="col-lg-2 col-sm-6 col-md-4 mt-3">
+                <img src="https://source.unsplash.com/1200x700?prgramming" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-2  col-sm-6 col-md-4 mt-3">
+                <img src="https://source.unsplash.com/1200x700?prgramming" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-2  col-sm-6 col-md-4 mt-3">
+                <img src="https://source.unsplash.com/1200x700?prgramming" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-2  col-sm-6 col-md-4 mt-3">
+                <img src="https://source.unsplash.com/1200x700?prgramming" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-2  col-sm-6 col-md-4 mt-3">
+                <img src="https://source.unsplash.com/1200x700?prgramming" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-2  col-sm-6 col-md-4 mt-3">
+                <img src="https://source.unsplash.com/1200x700?prgramming" alt="" class="img-fluid">
+            </div>
+      </div>
+
+      <div class="row mt-2 justify-content-center">
+          <div class="col-lg-3 col-md-6 mt-3">
+              <img src="https://source.unsplash.com/1200x700?personal" alt="" class="img-fluid">
+          </div>
+          <div class="col-lg-3 col-md-6 mt-3">
+              <img src="https://source.unsplash.com/1200x700?personal" alt="" class="img-fluid">
+          </div>
+          <div class="col-lg-3 col-md-6 mt-3">
+              <img src="https://source.unsplash.com/1200x700?personal" alt="" class="img-fluid">
+          </div>
+          <div class="col-lg-3 col-md-6 mt-3">
+              <img src="https://source.unsplash.com/1200x700?personal" alt=""  class="img-fluid">
+          </div>
+      </div>
+
+      <div class="row py-3 ">
+            <div class="col-lg-4 offset-lg-2 col-md-5 offset-md-1  mt-3">
+               <img src="https://source.unsplash.com/1200x700?gunung" alt=""  class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-5 mt-3">
+               <img src="https://source.unsplash.com/1200x700?gunung" alt=""  class="img-fluid">
+            </div>
+      </div>
+
+  </div>
+</div>
+
+
+  <div class="container-fluid bg-dark">
+      <div class="container">
+
+        <div class="row py-3">
+            <div class="col-12">
+            <h3 class="text-center text-white">Layout Website</h3>
+            <p class="text-center text-white">With Grid System Bootstrap 5</p>
+            </div>
+        </div>
+
+        <div class="row align-items-center py-3">
+
+            <div class="col-lg-4 col-md-6">
+                <img src="https://source.unsplash.com/1200x700?manusia" alt="" class="img-fluid img-circle">
+            </div>
+
+            <div class="col-lg-4 col-md-6 offset-lg-1 mt-3">
+            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, doloremque eius? Assumenda odit at aliquam voluptas sapiente obcaecati unde voluptatum eaque ex, libero enim, nostrum corrupti repellat ad repudiandae saepe maxime porro! Provident ipsam omnis, facere culpa molestiae animi consequatur.</p>
+            </div>
+
+            <div class="col-lg-2 col-md-12 offset-lg-1 mt-3 text-center">
+            <button class="btn btn-primary btn-sm">Let's Start</button>
+            </div>
+
+
+        </div>
+      </div>
+  </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
